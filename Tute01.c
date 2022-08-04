a0fa9827c7c5343aa480f60b33227896f43e467c
Jaliyamy <110586465+Jaliyamy@users.noreply.github.com>
2022-08-04 15:14:14 +0530

    Answer Tutorial 1

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,19 @@
 #include <stdio.h>
 
 int main() {
+     float M1,M2,average,sum;
+  
+  printf("Enter Mark :\n");
+  scanf("%f",&M1);
+
+  printf("Enter Mark :\n");
+  scanf("%f",&M2);
+
+  sum=M1+M2;
+  average=sum/2;
+
+  printf("Your Average is %.2f :\n",average);
+  return 0;
   
   return 0;
 }
